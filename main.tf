2ba922d5833992a01ab1d4f9dcad3c8792928894
Bacsai Peter <peter.bacsai@sap.com>
2025-06-13 16:47:17 +0200

    fixing the mount of the bucket.

--- main.tf
+++ main.tf
@@ -1,19 +1,34 @@
 resource "google_storage_bucket" "scripts" {
-  name     = "${var.project_id}-ubuntu-scripts"
-  location = "EU"
+  name          = "${var.project_id}-ubuntu-scripts"
+  location      = "EU"
   force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+# Add IAM binding for the VM service account to access the bucket
+resource "google_storage_bucket_iam_member" "vm_access" {
+  bucket = google_storage_bucket.scripts.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_compute_instance.ubuntu_vm.service_account[0].email}"
 }
 
 resource "google_storage_bucket_object" "init_script" {
   name   = "init-script.sh"
   bucket = google_storage_bucket.scripts.name
   source = "scripts/init-script.sh"
+  content_type = "text/x-shellscript"
 }
 
 resource "google_compute_instance" "ubuntu_vm" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
+
+  service_account {
+    email  = google_service_account.vm_service_account.email
+    scopes = ["cloud-platform"]
+  }
 
   boot_disk {
     initialize_params {
@@ -61,7 +76,21 @@ resource "google_compute_instance" "ubuntu_vm" {
   EOF
 
   tags = var.network_tags
-} 
+}
+
+# Create a service account for the VM
+resource "google_service_account" "vm_service_account" {
+  account_id   = "vm-service-account"
+  display_name = "VM Service Account"
+  project      = var.project_id
+}
+
+# Grant the service account necessary roles
+resource "google_project_iam_member" "vm_service_account_storage" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.vm_service_account.email}"
+}
 
 resource "google_compute_network" "vpc_network" {
   name                    = "ubuntu-vpc-network"
